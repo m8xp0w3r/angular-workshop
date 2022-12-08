1e7e8c315415d957ea1b10f5809ba2bc87f40d39
Tim <tim.schreindl@cn-consult.eu>
2022-12-08 15:30:30 +0100

    Added pipe

--- libs/demo/src/lib/demo.module.ts
+++ libs/demo/src/lib/demo.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemoViewComponent } from './demo-view/demo-view.component';
 import { DemoDirective } from './demo.directive';
+import { DemoPipe } from './demo.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [DemoViewComponent, DemoDirective],
+  declarations: [DemoViewComponent, DemoDirective, DemoPipe],
   exports: [DemoViewComponent],
 })
 export class DemoModule {}
---
+++ libs/demo/src/lib/demo.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'demo'
+})
+export class DemoPipe implements PipeTransform {
+
+  transform(value: string, ...args: unknown[]): string {
+    switch (value)
+    {
+      case "small":
+        return "kleine ";
+
+      case "medium":
+        return "mittelere ";
+
+      case "large":
+        return "große ";
+
+      default:
+        return "keine ahnung";
+    }
+  }
+
+}
