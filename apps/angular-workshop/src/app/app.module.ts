5d84d9f961f17233753bb9c503ed25848fa2a175
Nils Leistner <nils.leistner@cn-consult.eu>
2022-12-09 13:12:37 +0100

    Firebase angemeldet

--- apps/angular-workshop/src/app/app.module.ts
+++ apps/angular-workshop/src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/angular-workshop/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-workshop-f385e',
+    appId: '1:320011829902:web:e34a7fa69c765a59813fdf',
+    storageBucket: 'angular-workshop-f385e.appspot.com',
+    apiKey: 'AIzaSyD31JlhFEaiCJNXUlqvB9TVkICXkEkrbpE',
+    authDomain: 'angular-workshop-f385e.firebaseapp.com',
+    messagingSenderId: '320011829902',
+  },
+  production: true,
+};
