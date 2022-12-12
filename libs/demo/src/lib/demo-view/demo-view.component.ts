ff7fd591a41f6d3f09d0f2633a0ae79d6861f3be
Nils Leistner <nils.leistner@cn-consult.eu>
2022-12-12 11:02:06 +0100

    use firebase service

--- libs/demo/src/lib/demo-view/demo-view.component.ts
+++ libs/demo/src/lib/demo-view/demo-view.component.ts
@@ -1,95 +1,72 @@
-import {
-  AfterViewInit,
-  Component,
-  ComponentRef,
-  ElementRef,
-  EventEmitter,
-  Input,
-  QueryList,
-  ViewChild,
-  ViewChildren
-} from '@angular/core';
-import {DemoService} from "../demo.service";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {FirebaseService} from "../../../../firebase/src/lib/firebase.service";
-import {MatCheckboxChange} from "@angular/material/checkbox";
-import {MatSort} from "@angular/material/sort";
-import {MatTable, MatTableDataSource} from "@angular/material/table";
-import {Subject} from "rxjs";
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { DemoService } from "../demo.service";
+import { FormControl, FormGroup } from "@angular/forms";
+import { FirebaseService } from "../../../../firebase/src/lib/firebase.service";
+import { MatCheckboxChange } from "@angular/material/checkbox";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
 
 interface Validator<T extends FormControl> {
-  (c:T): {[error: string]:any};
+	(c: T): { [error: string]: any };
 }
 
 export interface Player {
-  id: number;
-  name: string;
-  progress: number;
-  color: string;
+	id: number;
+	name: string;
+	progress: number;
+	color: string;
 }
 
 
-
 @Component({
-  selector: 'diloc-demo-view',
-  templateUrl: './demo-view.component.html',
-  styleUrls: ['./demo-view.component.scss'],
+	selector: 'diloc-demo-view',
+	templateUrl: './demo-view.component.html',
+	styleUrls: ['./demo-view.component.scss'],
 })
 
 
+export class DemoViewComponent implements OnInit, AfterViewInit {
+	@Input() listItems: string[] = [];
 
-export class DemoViewComponent implements AfterViewInit {
-  @Input() listItems: string[] = [];
+	@ViewChild(MatSort) sort!: MatSort;
 
-  @ViewChild(MatSort) sort!: MatSort;
+	displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
+	dataSource = new MatTableDataSource();
 
-  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
-  dataSource = new MatTableDataSource();
+	newPlayerForm = new FormGroup({
+		name: new FormControl(),
+		color: new FormControl()
+	});
 
-  newPlayerForm = new FormGroup({
-    name: new FormControl(),
-    color: new FormControl()
-  });
+	currentColor = "";
 
-  entries: string[] | undefined;
+	constructor(private demoService: DemoService, private fireBaseService: FirebaseService) {
+	}
 
-  subject = new Subject<Player>();
+	ngOnInit(): void {
+		this.fireBaseService.getCollection("nils").subscribe(next => {
+			this.dataSource.data = next;
+		});
+	}
 
-  currentColor = "";
+	ngAfterViewInit() {
+		this.dataSource.sort = this.sort;
 
-  constructor(private demoService: DemoService, private fireBaseService: FirebaseService) {
-  }
+	}
 
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
+	addEntry() {
+		const entry = {
+			id: this.dataSource.data.length + 1,
+			name: this.newPlayerForm.controls.name.value,
+			progress: Math.round(Math.random() * 100),
+			color: this.currentColor
+		};
+		void this.fireBaseService.addItem("nils", entry);
+	}
 
-    this.subject.asObservable().subscribe(next =>
-    {
-      this.dataSource.data = [...this.dataSource.data, next];
-    });
-  }
-
-  clicked() {
-    this.demoService.addEntry("test");
-    this.entries = this.demoService.getEntries();
-  }
-
-  addEntry() {
-    const entry = {
-      id: this.dataSource.data.length + 1,
-      name: this.newPlayerForm.controls.name.value,
-      progress: Math.round(Math.random() * 100),
-      color: this.currentColor
-    };
-
-    this.subject.next(entry);
-  }
-
-  onColorChange(event: MatCheckboxChange)
-  {
-    if (event.checked)
-    {
-      this.currentColor = event.source.value;
-    }
-  }
+	onColorChange(event: MatCheckboxChange) {
+		if (event.checked) {
+			this.currentColor = event.source.value;
+		}
+	}
 }
